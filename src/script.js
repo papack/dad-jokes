a41e36d53f0a2e00e26b10ac947d934cce5cd3ce
Matthias Steiner <m@kjpjm.de>
2021-01-12 23:24:36 +0100

    change script to async await

--- src/script.js
+++ src/script.js
@@ -5,14 +5,15 @@ generateJoke();
 
 jokeBtn.addEventListener("click", generateJoke);
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((res) => res.json())
-    .then((data) => (jokeEl.innerHTML = data.joke));
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
 }
